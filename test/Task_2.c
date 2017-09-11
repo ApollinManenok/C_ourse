b44b81db74e8fae7ec59a30c75e67f6eda1ca665
Polina Manenok <Apollinarya-STEP@users.noreply.github.com>
2017-09-11 11:51:12 +0300

    task_2?

--- test/Task_2.c
+++ test/Task_2.c
@@ -1,34 +1,82 @@
 #include<stdio.h>
 #include<stdlib.h>
-//vstavkoj!
-main()
+#include<stdbool.h>
+#include<time.h>
+void print_m(int array[], int x)
 {
-	int array[10] = { 4, 7, 2, 9, 3, 8, 5, 1, 6, 0 };
-	int ind_sort = 0;//granica otsortirovannogo
-	int num = 0;
-	for (int i = 1; i < 10; i++)
+	for (int i = 0; i < x; i++)
 	{
-		//array[i] chislo dl'a perenosa
-		if (array[i] >= array[i-1])
+		printf("%d ", array[i]);
+	}
+	printf("\n\n");
+}
+void acting(int array1[], int a, int array2[], int b, int* p, int ind)
+{
+	for (int i = 0; i < a; i++)
+	{
+		for (int i1 = 0; i1 < b; i1++)
 		{
-			ind_sort++;
-			continue;
-		}
-		num = array[i];
-		for (int i2 = ind_sort; i2 >= 0; i2--)//poisk mesta po otsortirovannomu
-		{
-			if (num < array[i2])
+			//printf("i = %d, i1 = %d\n", i , i1);
+			if (array1[i] == array2[i1])
 			{
-				array[i2 + 1] = array[i2];
-
+				break;
+			}
+			if (i1 == b - 1)
+			{
+				for (int i2 = 0; i2 < ind; i2++)
+				{
+					if (array1[i] == array3[i2])
+					{
+						//printf("array1[i] = %d  array3[i2] = %d  i = %d  i2 = %d break \n", array1[i], array3[i2], i, i2);
+						break;
+					}
+					if (i2 == ind - 1)
+					{
+						//printf("array1[i] = %d  i = %d  i2 = %d\n", array1[i], i, i2);
+						array3[i2] = array1[i];
+						ind++;
+						break;
+					}
+				}
 			}
 		}
-		for (int i1 = 0; i1 < 10; i1++)
-		{
-			printf("%d ", array[i1]);
-		}
 	}
-	printf("\n");
+}
+
+int main()
+{
+	srand(time(NULL));
+	int array1[100] = {0};
+	int array2[100] = {0};
+	int a = 1;
+	int b = 1;
+	
+	while (true)
+	{
+		int ind = 1;
+		int array3[10] = {0};
+		int*p = array3;
+		printf("Enter length for first massive ");
+		scanf_s("%d", &a);
+		printf("Enter length for second massive ");
+		scanf_s("%d", &b);
+		for (int i = 0; i < a; i++)
+		{
+			array1[i] = rand() % 20 + 1;
+		}
+		for (int i = 0; i < b; i++)
+		{
+			array2[i] = rand() % 20 + 1;
+		}
+		//acting(array1, a, array2, b, p, ind);
+
+		//print_m(array1, a);
+		//print_m(array2, b);
+		//print_m(array3, ind-1);
+	}
+	
+
+	
 	system("pause");
 	return 0;
 }
