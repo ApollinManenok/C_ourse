d33fcac866b0c530796d7bc501349c4734b0c10c
Polina Manenok <Apollinarya-STEP@users.noreply.github.com>
2017-09-12 11:46:02 +0300

    12_09_17_task4

---
+++ test/12_09_17/Tasks.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include"module.h"
+#include<time.h>
+
+// task 1
+void task1()
+{
+
+}
+
+
+// task 2
+
+void task2()
+{
+	
+}
+
+// task 3
+void task3()
+{
+
+}
+
+// Task 4
+
+void task4()
+{
+	
+}
+
+/* Task 5
+
+
+
+void task5()
+{
+
+}
+*/
